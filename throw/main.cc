a58b0a833b000c94c2d40b5b074948bb480def9f
iydia <hly.liya@gmail.com>
2024-12-07 15:17:59 -0500

    fib with throwing

---
+++ throw/main.cc
@@ -0,0 +1,49 @@
+import <iostream>;
+import <stdexcept>;
+
+using namespace std;
+
+void fib(int n) {
+    if (n == 0 || n == 1) {
+        throw n;
+    }
+    // Note: fib has no return value, so the only way to grab the value is to try catch them recursively
+    try {
+        fib(n - 1);
+    } catch (int a) {
+        try {
+            fib(n - 2);
+        } catch (int b) {
+            throw a + b;
+        }
+    }
+}
+
+int main() {
+    int a = 2;
+    int b = 5;
+
+    try {
+        fib(a);
+    } catch (int e) {
+        cout << e << endl;
+    }
+
+    try {
+        fib(b);
+    } catch (int e) {
+        cout << e << endl;
+    }
+
+    try {
+        fib(6);
+    } catch (int e) {
+        cout << e << endl;
+    }
+
+    try {
+        fib(7);
+    } catch (int e) {
+        cout << e << endl;
+    }
+}
