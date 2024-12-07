5a15a63c9c020087b2f1e71aa7df65ed4d7f2086
iydia <hly.liya@gmail.com>
2024-12-07 15:20:35 -0500

    oneliner

--- throw/main.cc
+++ throw/main.cc
@@ -47,3 +47,5 @@ int main() {
         cout << e << endl;
     }
 }
+
+// import <iostream>; import <stdexcept>; using namespace std; void fib(int n) { if (n == 0 || n == 1) { throw n; } try { fib(n - 1); } catch (int a) { try { fib(n - 2); } catch (int b) { throw a + b; } } } int main() { int a = 2; int b = 5; try { fib(a); } catch (int e) { cout << e << endl; } try { fib(b); } catch (int e) { cout << e << endl; } try { fib(6); } catch (int e) { cout << e << endl; } try { fib(7); } catch (int e) { cout << e << endl; } }
