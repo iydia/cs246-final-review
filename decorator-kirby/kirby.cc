182254296c9a95c9972d88a0a983b6d1811df5bb
iydia <hly.liya@gmail.com>
2024-12-08 00:15:51 -0500

    failing the final man

--- decorator-kirby/kirby.cc
+++ decorator-kirby/kirby.cc
@@ -30,9 +30,34 @@ public:
 };
 
 // Fill in concrete decorator for Fire ability
+class Fire : public Decorator {
+    public:
+    Fire(std::shared_ptr<Character> c) : Decorator{c} {}
+    void action() const override {
+        Decorator::action();
+        std::cout << "Kirby uses Fire ability!" << std::endl;
+    }
+};
 
-// Fill in oncrete decorator for Ice ability
+// Fill in concrete decorator for Ice ability
+class Ice : public Decorator {
+    public:
+    Ice();
+    Ice(std::shared_ptr<Character> c) : Decorator{c} {}
+    void action() const override {
+        Decorator::action();
+        std::cout << "Kirby uses Ice ability!" << std::endl;
+    }
+};
 
 int main() {
     // Fill in with action() commands
+    std::shared_ptr<Character> kirby = std::make_shared<Kirby>();
+    std::shared_ptr<Character> fireKirby = std::make_shared<Fire>(kirby);
+    std::shared_ptr<Character> iceKirby = std::make_shared<Ice>(kirby);
+    std::shared_ptr<Character> fireIceKirby = std::make_shared<Ice>(std::make_shared<Fire>(kirby)); // nested fire and ice kirby
+    kirby->action();
+    fireKirby->action();
+    iceKirby->action();
+    fireIceKirby->action();
 }
